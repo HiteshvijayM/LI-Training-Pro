22f39aa238e782fb1768ed8034b322d4c0a4b6b9
HiteshvijayM <hiteshv@maqsoftware.com>
2022-06-14 11:07:52 +0530

    Commit 13/06 final

--- src/ExceptionHandling/Quiz.java
+++ src/ExceptionHandling/Quiz.java
@@ -1,25 +1,26 @@
-package ExceptionHandling;
+// package ExceptionHandling;
 
-public class Quiz {
-    public static void main(String[] args) {
+// public class Quiz {
+//     public static void main(String[] args) {
         
     
-    try{
-        int x = 0;
-        int y = 5/x;
+//     try{
+//        // int x = 0;
+//        // int y = 5/x;
 
 
-    }
-    catch (Exception e) {
-        System.out.println("exception");
-    }
-    // catch (ArithmeticException e1){
-    //     System.out.println("arithematic caught");
-    // }
+//     }
+//     catch (Exception e) {
+//         System.out.println("exception");
+//     }
+//     // catch (ArithmeticException e1){
+//     //     System.out.println("arithematic caught");
+//     // }
 
-    finally{
-        System.out.println("finally");
+//     finally{
+//         System.out.println("finally");
 
-    }
+//     }
 
-}}
+// }
+// }
